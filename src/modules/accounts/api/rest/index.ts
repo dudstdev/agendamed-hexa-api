11e21b8a38a7a18933c45d7e923316529e22b6c1
Eduardo Tavares <eduardot.santos@telefonicati.onmicrosoft.com>
2025-07-10 01:30:00 -0300

    feat: add controllers and services for account management and password reset functionality

---
+++ src/modules/accounts/api/rest/index.ts
@@ -0,0 +1,14 @@
+export * from "./create-account/create-account.impl.controller";
+export * from "./create-account/create-account.controller";
+
+export * from "./request-account-confirmation/request-account.confirmation.impl.controller";
+export * from "./request-account-confirmation/request-account.confirmation.controller";
+
+export * from "./confirm-account/confirm-account.impl.controller";
+export * from "./confirm-account/confirm-account.controller";
+
+export * from "./request-password-reset/request-password-reset.impl.controller";
+export * from "./request-password-reset/request-password-reset.controller";
+
+export * from "./reset-password/reset-password.impl.controller";
+export * from "./reset-password/reset-password.controller";
---
+++ src/modules/accounts/core/port/in/index.ts
@@ -0,0 +1,11 @@
+export * from "./dto/account-confirmation.dto";
+export * from "./dto/account.dto";
+export * from "./dto/confirm-account.dto";
+export * from "./dto/password-reset.dto";
+export * from "./dto/reset-password.dto";
+
+export * from "./create-account.port-in";
+export * from "./confirm-account.port-in";
+export * from "./request-account-confirmation.port-in";
+export * from "./request-password-reset.port-in";
+export * from "./reset-password.port-in";
