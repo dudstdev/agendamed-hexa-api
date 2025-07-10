0d507fa2076792f0bcca52982470c94581f5f3b9
Eduardo Tavares <eduardot.santos@telefonicati.onmicrosoft.com>
2025-07-10 01:24:52 -0300

    feat: implement reset password service and related DTOs

---
+++ src/modules/accounts/core/application/service/reset-password.service.ts
@@ -0,0 +1,53 @@
+import { Inject } from "@nestjs/common";
+
+import { TokenType } from "@/accounts/core/domain";
+import { ResetPasswordDTO, ResetPasswordPortIn } from "@/accounts/core/port/in";
+import {
+  AccountsPortOut,
+  HashGeneratorPortOut,
+  TokensPortOut,
+} from "@/accounts/core/port/out";
+
+export class ResetPasswordService implements ResetPasswordPortIn {
+  constructor(
+    @Inject("TokensPortOut")
+    private readonly tokensPortOut: TokensPortOut,
+
+    @Inject("AccountsPortOut")
+    private readonly accountsPortOut: AccountsPortOut,
+
+    @Inject("HashGeneratorPortOut")
+    private readonly hashGeneratorPortOut: HashGeneratorPortOut,
+  ) {}
+
+  async execute({ code, newPassword }: ResetPasswordDTO): Promise<void> {
+    const resetToken = await this.tokensPortOut.findByCode(code);
+
+    if (!resetToken) {
+      throw new Error("Unauthorized");
+    }
+
+    if (resetToken.isExpired()) {
+      throw new Error("Unauthorized");
+    }
+
+    if (resetToken.type !== TokenType.PASSWORD_RESET) {
+      throw new Error("Unauthorized");
+    }
+
+    const account = await this.accountsPortOut.findById(
+      resetToken.accountId.toString(),
+    );
+
+    if (!account) {
+      throw new Error("Unauthorized");
+    }
+
+    const hashedPassword = await this.hashGeneratorPortOut.hash(newPassword);
+
+    account.password = hashedPassword;
+
+    await this.accountsPortOut.save(account);
+    await this.tokensPortOut.delete(resetToken);
+  }
+}
